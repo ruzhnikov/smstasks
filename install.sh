8f5815e501b1a3862cc5afd3d3f6f469bf214346
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2014-04-19 21:26:44 +0400

    * checking dependings in install script * checking previous version

--- install.sh
+++ install.sh
@@ -9,25 +9,47 @@ ADDITIOANL_PATH="additional_scripts"
 CHK_DEP_SCRIPT="check_depending.pl"
 PERL="/usr/bin/perl"
 
+CONFIG_DIR="/etc/smstasks"
+CONFIG_NAME="smstasks.conf"
+
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
 
 set -e
 set -u
 
-function chk_fail {
-    if [ "$1" -ne 0 ]; then
-        echo -e "\nLast process return error, program will terminate...\n"
-        exit 1
-    fi
-}
 
 function chk_os {
     if ! [ -e /etc/debian_version ]; then
         echo "Your operation system are not Debian based. You must manually set the following programs:"
-        echo -e "Redis-server (>=2.6.12)\nMysql or MariaDB\n"
+        echo "Redis-server (>=2.6.12)"
+        echo "Mysql-client or MariaDB-client"
+        echo -e "Mysql-server or MariaDB-server (if necessary)\n"
         return 1
     else
         return 0
     fi
+}
+
+function echo_fail {
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+}
+
+function echo_ok {
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+}
+
+function upgrade {
+    echo "Success!"
+    return 0
 }
 
 echo -e "\nChecking required perl modules...\n"
@@ -35,38 +57,46 @@ $PERL $PATH_DIR/$ADDITIOANL_PATH/$CHK_DEP_SCRIPT
 
 echo -e "\nChecking required programms...\n"
 
-missing_packages=0
-
 if chk_os
 then
-    echo -ne "Checking redis-server...\t"
+    missing_packages=0
+
+    echo -ne "Checking redis-server..."
     PKG_REDIS=$(dpkg-query -W --showformat='${Status}\n' redis-server|grep "install ok installed")
+    PKG_REDIS_VERSION=$(dpkg-query -W --showformat='${Version}\n' redis-server )
+    version_pattern="^[0-9]+\:2\.[68]"
     if [ "$PKG_REDIS" == "" ]; then
         missing_packages=`expr $missing_packages + 1`
-        echo "[fail]"
+        echo_fail
     else
-        echo "[ok]"
+        if [[ "$PKG_REDIS_VERSION" =~ $version_pattern ]]; then
+            echo_ok
+        else
+            missing_packages=`expr $missing_packages + 1`
+            echo_fail
+            echo "version of redis-server must be >= 2.6"
+        fi
     fi
 
-    echo -ne "Checking mysql-server...\t"
-    PKG_MYSQL=$(dpkg-query -W --showformat='${Status}\n' mysql-server-*|grep "install ok installed" | uniq)
-    PKG_MARIADB=$(dpkg-query -W --showformat='${Status}\n' mariadb-server-*|grep "install ok installed" | uniq)
-    
-    if [ "$PKG_MYSQL" == "" ]; then
-        echo "[fail]"
-        echo -ne "Checking mariadb-server...\t"
-        if [ "$PKG_MARIADB" == "" ]; then
+    echo -ne "Checking mysql-client..."
+    PKG_MYSQL_CLIENT=$(dpkg-query -W --showformat='${Status}\n' mysql-client-*|grep "install ok installed" | uniq)
+    PKG_MARIADB_CLIENT=$(dpkg-query -W --showformat='${Status}\n' mariadb-client-*|grep "install ok installed" | uniq)
+
+    if [ "$PKG_MYSQL_CLIENT" == "" ]; then
+        echo_fail
+        echo -ne "Checking mariadb-client..."
+        if [ "$PKG_MARIADB_CLIENT" == "" ]; then
             missing_packages=`expr $missing_packages + 1`
-            echo "[fail]"
+            echo_fail
         else
-            echo "[ok]"
+            echo_ok
         fi
     else
-        echo "[ok]"
+        echo_ok
     fi
 
     if [ "$missing_packages" -gt "0" ]; then
-        echo "found not installed packages."
+        echo -e "\nFound not installed packages."
         echo "You need to install the missing packages and re-run the script install.sh"
         exit 1
     fi
@@ -75,7 +105,32 @@ fi
 echo -e "\nInstallation...\n"
 echo -e "Checking the previous version...\n"
 
-# проверяем ранее установленную версию
-# ...
+if [[ -e "$PURPOSE_DIR/$PROGRAM_NAME" && -d "$PURPOSE_DIR/$PROGRAM_NAME" ]]
+then
+    echo "Found directory $PURPOSE_DIR/$PROGRAM_NAME"
+
+    if [ -e "$PURPOSE_DIR/$PROGRAM_NAME/bin/$PROGRAM_NAME.pl" ]; then
+        cur_version=$(grep 'my $VERSION' $PURPOSE_DIR/$PROGRAM_NAME/bin/$PROGRAM_NAME.pl | sed -r 's/^.*([0-9]+\.[0-9]+).*$/\1/')
+        my_version=$($PERL bin/$PROGRAM_NAME.pl --version)
+        echo "Version of the installed program $cur_version"
+        echo "New version $my_version"
+    fi
+
+    while [ true ]
+    do
+        echo "You want update program? [Y/n]: "
+        read item
+        case "$item" in
+            y|Y) upgrade
+                break
+                ;;
+            n|N) echo "Exit"
+                exit 0
+                ;;
+        esac
+    done
+else
+    echo -e "Previously installed versions of the program not found\n"
+fi
 
 exit 0
